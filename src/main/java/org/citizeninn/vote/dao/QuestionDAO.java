7ef0c0610b7730d7a5e961e0e7df60c8877cdc6f
borys86 <borys86@gmail.com>
2013-07-31 20:41:15 +0200

    Added DOA object for Question

---
+++ src/main/java/org/citizeninn/vote/dao/QuestionDAO.java
@@ -0,0 +1,10 @@
+package org.citizeninn.vote.dao;
+
+import org.citizeninn.dao.GenericDAO;
+import org.citizeninn.vote.Answer;
+import org.citizeninn.vote.Question;
+
+public interface QuestionDAO extends GenericDAO<Question, Long> {
+	Answer getLatestAnswer(Long questionId);
+
+}
