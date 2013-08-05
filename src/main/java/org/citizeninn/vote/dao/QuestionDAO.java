bc6a9a97a9b2a43fb048f3305423cbb56414d613
borys86 <borys86@gmail.com>
2013-08-05 21:02:13 +0100

    added QuestionDAO

--- src/main/java/org/citizeninn/vote/dao/QuestionDAO.java
+++ src/main/java/org/citizeninn/vote/dao/QuestionDAO.java
@@ -5,6 +5,7 @@ import org.citizeninn.vote.Answer;
 import org.citizeninn.vote.Question;
 
 public interface QuestionDAO extends GenericDAO<Question, Long> {
+
 	Answer getLatestAnswer(Long questionId);
 
 }
